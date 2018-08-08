1c7ed46991548baa6d0797ca88512ae36daf7ed0
arkadzi.daniyelian <arkadzi.daniyelian@accenture.com>
2018-08-08 10:50:27 +0300

    initial

---
+++ AzureMessaging/AzureMessaging.h
@@ -0,0 +1,19 @@
+//
+//  AzureMessaging.h
+//  AzureMessaging
+//
+//  Created by ark dan on 08/08/2018.
+//  Copyright © 2018 arkdan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AzureMessaging.
+FOUNDATION_EXPORT double AzureMessagingVersionNumber;
+
+//! Project version string for AzureMessaging.
+FOUNDATION_EXPORT const unsigned char AzureMessagingVersionString[];
+
+
+#import <AzureMessaging/SBNotificationHub.h>
+#import <AzureMessaging/SBConnectionString.h>
